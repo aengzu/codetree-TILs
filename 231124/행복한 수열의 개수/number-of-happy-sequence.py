f98dc852e8e71aa5f36dbba984597364c20d743e
aengzu <102356873+aengzu@users.noreply.github.com>
2023-11-24 14:38:41 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231124/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,40 @@
+n, m = tuple(map(int, input().split()))
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+ans = 0
+cnt = [
+    0
+    for _ in range(n)
+]
+
+for i in range(n):
+    cnt = [1,1,1]
+    #행
+    for j in range(n-1):
+        
+        #열
+        if arr[i][j] == arr[i][j+1]:
+            cnt[j] += 1
+    for k in range(n):
+        if cnt[k]>=m:
+            ans += 1
+            break
+
+for j in range(n):
+    cnt = [1,1,1]
+    #행
+    for i in range(n-1):
+        
+        #열
+        if arr[i][j] == arr[i+1][j]:
+            cnt[j] += 1
+   
+    for k in range(n):
+        if cnt[k]>=m:
+            ans += 1
+            break
+
+print(ans)
