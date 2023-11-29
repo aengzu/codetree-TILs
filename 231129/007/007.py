aa4069e1ffccf03b582b31d6f136633abbf140c9
aengzu <102356873+aengzu@users.noreply.github.com>
2023-11-29 11:20:31 +0900

    Title: 007, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 231129/007/007.py
+++ 231129/007/007.py
@@ -11,6 +11,12 @@ s_code, m_point, time = tuple(input().split())
 s = Spy(s_code, m_point, int(time))
 
 
+# tuple 생성
+#s = (s_code, m_point, int(given_time))
+
+# tuple 원소들을 각 변수에 대입
+#secret_code, meeting_point, time = s
+
 print(f"secret code : {s.secret_code}")
 print(f"meeting point : {s.meeting_point}")
 print(f"time : {s.time}")
