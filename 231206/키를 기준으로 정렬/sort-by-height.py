14a41b73a7a155a3324958f8f372fa2e73378a08
aengzu <102356873+aengzu@users.noreply.github.com>
2023-12-06 18:42:51 +0900

    Title: 키를 기준으로 정렬, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231206/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+n = int(input())
+
+students = []
+
+
+for _ in range(n):
+    name, height, weight = list(input().split())
+    students.append(Student(name, height, weight))
+
+
+students.sort(key=lambda x: x.height)
+
+for student in students:
+    print(student.name, student.height, student.weight)
