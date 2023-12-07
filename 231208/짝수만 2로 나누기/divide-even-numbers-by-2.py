c13f02fbaed8f064617f48fe718ad115e73bf79a
aengzu <102356873+aengzu@users.noreply.github.com>
2023-12-08 01:21:50 +0900

    Title: 짝수만 2로 나누기, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 231208/짝수만 2로 나누기/divide-even-numbers-by-2.py
+++ 231208/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -1,11 +1,14 @@
+# 변수 선언 및 입력:
 n = int(input())
 arr = list(map(int, input().split()))
 
-def print_arr(arr):
-    for  elem in arr:
-        if elem%2==0:
-            print(int(elem/2), end=' ')
-        else:
-            print(elem, end=' ')
 
-print_arr(arr)
+def modify(arr):
+    for i in range(n):
+        if arr[i] % 2 == 0:
+            arr[i] //= 2
+
+
+modify(arr)
+for elem in arr:
+    print(elem, end=" ")
