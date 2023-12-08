6beced7dddc2a098fa2fb3f5c249260fd031f3ee
aengzu <102356873+aengzu@users.noreply.github.com>
2023-12-08 13:33:41 +0900

    Title: 2개 이상의 알파벳, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 231208/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 231208/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -2,6 +2,9 @@ st = input()
 
 def is_other_alpha_morethantwo(st):
     for i in range(len(st)):
+        # 돌아가면서 첫번째 원소와 같지 않다면 True 를 리턴
+        # 만약 다 돌았는데 if 문에 들어가지 않는다는 것은
+        # 모든 원소들이 첫번째 원소와 같다 즉, 모든 원소가 같다는 뜻이므로 False
         if st[i] != st[0]:
             return True
     
