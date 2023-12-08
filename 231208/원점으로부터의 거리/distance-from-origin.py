478b760b3e54723cf6cffb5d03e5d39b387218b3
aengzu <102356873+aengzu@users.noreply.github.com>
2023-12-08 13:16:55 +0900

    Title: 원점으로부터의 거리, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231208/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,17 @@
+class Point:
+    def __init__(self, x, y, num):
+        self.x = x
+        self.y = y
+        self.num =num
+
+n = int(input())
+points = []
+
+for i in range(1,n+1):
+    x, y = tuple(map(int, input().split()))
+    points.append(Point(x, y, i))
+
+points.sort(key=lambda c: (abs(c.x+c.y), c.num))
+
+for point in points:
+    print(point.num)
