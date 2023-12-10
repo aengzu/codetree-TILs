9a74873b6a4b15427827593d42a4ea2a29411247
aengzu <102356873+aengzu@users.noreply.github.com>
2023-12-11 00:29:52 +0900

    Title: 그 계절, 그 날, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231211/그 계절, 그 날/that-season-that-day.py
+++ 231211/그 계절, 그 날/that-season-that-day.py
@@ -21,6 +21,9 @@ def exist_date(y, m, d):
     if yunnyeon(y):
         if m==2 and d>30:
             return '-1'
+    else:
+        if m==2 and d>29:
+            return '-1'
     
     if 3 <= m and m <=5:
         return 'Spring'
