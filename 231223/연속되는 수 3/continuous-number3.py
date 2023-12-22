f1743601583a1d4766927041d48125567d319db4
aengzu <102356873+aengzu@users.noreply.github.com>
2023-12-23 00:06:21 +0900

    Title: 연속되는 수 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231223/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+# 동일 숫자로만 이루어진 연속 부분 수열
+
+arr = [ int(input()) 
+        for _ in range(n)]
+
+ans = 0
+for i in range(1, len(arr)):
+    # 부호 같을 때
+    if arr[i] * arr[i-1] > 0:
+        cnt += 1
+    else:
+        cnt = 1
+    ans = max(ans, cnt)
+    
+print(ans)
