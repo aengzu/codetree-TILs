54341b5aeda6db7cc5c2aa9e00f8a17c3d0e5004
aengzu <102356873+aengzu@users.noreply.github.com>
2023-12-22 17:23:46 +0900

    Title: 연속되는 수 2, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231222/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,16 @@
+# 숫자가 동일한 연속 부분 수열
+# 배열의 직전의 수와 현재 수가 다를 경우에만 개수를 증가시킨다.
+
+n = int(input())
+
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+cnt = 0 
+c=1
+for i in range(len(arr)):
+    if arr[i] == arr[i-1]:
+        c += 1
+    cnt = max(cnt,c)
+print(cnt)
