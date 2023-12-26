96226ff78314263b01b716b5a04adea2d70057e9
aengzu <102356873+aengzu@users.noreply.github.com>
2023-12-26 12:39:17 +0900

    Title: 벌금은 누구에게, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 231226/벌금은 누구에게/who-will-pay.py
+++ 231226/벌금은 누구에게/who-will-pay.py
@@ -3,9 +3,12 @@ n,m,k = list(map(int, input().split()))
 # 학생의 번호 배열
 students = [0] * (n+1)
 
+ans = -1
 for i in range(m):
     red = int(input())
     students[red] += 1
     if students[red] >= k:
-        print(red)
+        ans = red
         break
+
+print(ans)
