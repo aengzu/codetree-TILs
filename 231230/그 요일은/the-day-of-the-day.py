13b8a3b7d4343fdb53b6b92257c38cb8c998ed65
aengzu <102356873+aengzu@users.noreply.github.com>
2023-12-30 01:34:28 +0900

    Title: 그 요일은, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231230/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,22 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+A = input()
+
+days_of_weak = ["Mon", "Tue","Wed", "Thu", "Fri", "Sat", "Sum"]
+days_in_month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+
+def calculate_days(m1, d1):
+    sum = 0
+    for i in range(1, m1):
+        sum += days_in_month[m1]
+    sum += d1
+    return sum
+    
+diff = calculate_days(m2, d2) - calculate_days(m1, d1)
+
+rem = diff%7
+cals = diff//7
+
+if days_of_weak[rem]=='A':
+    cals += 1
+
+print(cals)
