25862fa34d49aff7760b0d624cacf1fd409216ba
aengzu <102356873+aengzu@users.noreply.github.com>
2024-01-17 19:46:32 +0900

    Title: 그래프 탐색, Time: 224ms, Memory: 26MB, Status: Passed - CodeTree

--- 240117/그래프 탐색/graph-traversal.py
+++ 240117/그래프 탐색/graph-traversal.py
@@ -4,8 +4,9 @@ n, m = tuple(map(int, input().split()))
 # index 를 1부터 사용하기 위해 
 graph = [[] for _ in range(n+1)]
 
-visited = [false] * (n+1)
+visited = [False for _ in range(n+1)]
 vertex_cnt = 0
+
 for _ in range(m):
     x, y = tuple(map(int, input().split()))
     graph[x].append(y)
