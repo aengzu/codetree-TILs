c5a6f65e57d30f79bc946e07754f3396f434c374
aengzu <102356873+aengzu@users.noreply.github.com>
2024-03-07 12:43:58 +0900

    Title: 두 선분, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/두 선분/two-lines.py
@@ -0,0 +1,14 @@
+x1, x2, x3, x4 = list(map(int, input().split()))
+
+def is_intersecting(x1, x2, x3, x4):
+    if x1<x3 and x3<=x2:
+        return True
+    elif x3<x1 and x1<=x4:
+        return True
+    else:
+        return False
+
+if(is_intersecting(x1,x2,x3,x4)):
+    print("intersecting")
+else:
+    print("nointersecting")
