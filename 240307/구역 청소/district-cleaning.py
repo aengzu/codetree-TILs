d383091dbfa9de93607a24d1c7f99783b2a78d91
aengzu <102356873+aengzu@users.noreply.github.com>
2024-03-07 20:52:19 +0900

    Title: 구역 청소, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/구역 청소/district-cleaning.py
@@ -0,0 +1,17 @@
+a, b = tuple(map(int, input().split()))
+c, d = tuple(map(int, input().split()))
+
+# 겹치지 않으면 b-a + d-c
+# 겹치면 끝점 - 첫점
+
+
+def is_intersecting(a,b,c,d):
+    if b < c or d < a:
+        return False
+    return True
+
+
+if(is_intersecting):
+    print(max(d-a, b-c))
+else:
+    print(b-a+d-c)
