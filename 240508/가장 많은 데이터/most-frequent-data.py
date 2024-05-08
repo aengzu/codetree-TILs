1551541b8049fc5767e1c9a4caf0593a50e5297b
aengzu <102356873+aengzu@users.noreply.github.com>
2024-05-08 17:25:51 +0900

    Title: 가장 많은 데이터, Time: 391ms, Memory: 37MB, Status: Passed - CodeTree

--- 240508/가장 많은 데이터/most-frequent-data.py
+++ 240508/가장 많은 데이터/most-frequent-data.py
@@ -2,18 +2,25 @@
 # => 시간복잡도 O(N)
 # 문자열이 만약 배열의 index 처럼 쓰인다면 dict 를 이용하여 O(1) 만에 찾을 수 있다.
 
+# 변수 선언 및 입력:
 n = int(input())
+words = [input() for _ in range(n)]
 
-colors = {}
-max_cnt = 0
+freq = dict()
+ans = 0
 
-for _ in range(n):
-    word = input()
-    if word not in colors:
-        colors[word] = 1
+# 각 문자열이 몇 번씩 나왔는지를
+# hashmap에 기록해줍니다.
+for word in words:
+    # 처음 나온 문자열이라면 1을 직접 적어줘야 합니다.
+    if word not in freq:
+        freq[word] = 1
+    # 이미 나와있던 문자열이라면 1을 더해줍니다.
     else:
-        colors[word] += 1
-        max_cnt = max(max_cnt, colors[word])
+        freq[word] += 1
 
+    # 가장 많이 나온 횟수를
+    # 갱신해줍니다.
+    ans = max(ans, freq[word])
 
-print(max_cnt)
+print(ans)
