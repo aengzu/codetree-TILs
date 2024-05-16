37d8af6efc49d5d043208a0f6b0e654450e11549
aengzu <102356873+aengzu@users.noreply.github.com>
2024-05-16 17:16:48 +0900

    Title: 데이터 비교, Time: 300ms, Memory: 41MB, Status: Passed - CodeTree

--- 240516/데이터 비교/data-comparison.py
+++ 240516/데이터 비교/data-comparison.py
@@ -5,8 +5,10 @@ s1 = list(map(int, input().split()))
 m = int(input())
 s2 = list(map(int, input().split()))
 
+set1 = set(s1)
+
 for elem in s2:
-    if elem in s1:
+    if elem in set1:
         print('1', end=' ')
     else:
         print('0', end=' ')
