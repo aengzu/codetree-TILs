54071ac8d53982ad75b12c14a8cb48f76ddbe35c
aengzu <102356873+aengzu@users.noreply.github.com>
2024-05-30 00:50:24 +0900

    Title: 되돌아오기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240530/되돌아오기/come-back.py
+++ 240530/되돌아오기/come-back.py
@@ -2,53 +2,45 @@ n = int(input())
 
 x, y = 0, 0
 
-# W, S, E, N
-dx, dy = [-1, 0, 1, 0],[0, -1, 0, 1]
+# 동 서 남 북
+dxs, dys = [1, -1, 0, 0], [0, 0, -1, 1]
 
-# 0: W, 1: S, 2:E, 3:N
+ans = -1
 
 time = 0
-c_flg = False
 
-def check(x,y):
-    return x==0 and y==0
-    
+# dir 방향으로 dist 만큼 이동
+def move(move_dir, dist):
+    global x, y
+    global ans, time
+
+    for _ in range(dist):
+        x, y = x + dxs[move_dir], y + dys[move_dir]
+
+        time += 1
+
+        if x==0 and y==0:
+            ans = time
+            return True
+    return False
+
 for _ in range(n):
-    dir, c = tuple(input().split())
-    c = int(c)
-    if dir=='N':
-        for i in range(c):
-            time += 1
-            x, y = x+dx[3], y+dy[3]
-            if check(x,y):
-                c_flg = True
-                print(time)
-                break
-    elif dir=='S':
-        for i in range(c):
-            time += 1
-            x, y = x+dx[1], y+dy[1]
-            if check(x,y):
-                c_flg = True
-                print(time)
-                break
-    elif dir=='E':
-        for i in range(c):
-            time += 1
-            x, y = x+dx[2], y+dy[2]
-            if check(x,y):
-                c_flg = True
-                print(time)
-                break
+    c_dir, dist = tuple(input().split())
+    dist = int(dist)
+
+    if c_idr == 'E':
+        move_dir =0
+    elif c_dir =='W':
+        move_dir = 1
+    elif c_dir =='S':
+        move_dir = 2
     else:
-        for i in range(c):
-            time += 1
-            x, y = x+dx[0], y+dy[0]
-            if check(x,y):
-                c_flg = True
-                print(time)
-                break
+        move_dir = 3
+    
+    done = move(move_dir, dist)
+
+    if done:
+        break
 
 
-if(c_flg==False):
-    print(-1)
+print(ans)
