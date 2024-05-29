5b50d5ed487ee54de1eeee7d74246d71b9ac7c7b
aengzu <102356873+aengzu@users.noreply.github.com>
2024-05-30 00:44:23 +0900

    Title: 되돌아오기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240530/되돌아오기/come-back.py
+++ 240530/되돌아오기/come-back.py
@@ -8,6 +8,7 @@ dx, dy = [-1, 0, 1, 0],[0, -1, 0, 1]
 # 0: W, 1: S, 2:E, 3:N
 
 time = 0
+c_flg = False
 
 def check(x,y):
     return x==0 and y==0
@@ -20,22 +21,30 @@ for _ in range(n):
             time += 1
             x, y = x+dx[3], y+dy[3]
             if check(x,y):
+                c_flg = True
                 print(time)
     elif dir=='S':
         for i in range(c):
             time += 1
             x, y = x+dx[1], y+dy[1]
             if check(x,y):
+                c_flg = True
                 print(time)
     elif dir=='E':
         for i in range(c):
             time += 1
             x, y = x+dx[2], y+dy[2]
             if check(x,y):
+                c_flg = True
                 print(time)
     else:
         for i in range(c):
             time += 1
             x, y = x+dx[0], y+dy[0]
             if check(x,y):
+                c_flg = True
                 print(time)
+
+
+if(c_flg==False):
+    print(-1)
