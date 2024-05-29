ac760ea7af3095a1bf58c8938dee01e05f59fa4f
aengzu <102356873+aengzu@users.noreply.github.com>
2024-05-30 00:42:57 +0900

    Title: 되돌아오기, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240530/되돌아오기/come-back.py
@@ -0,0 +1,41 @@
+n = int(input())
+
+x, y = 0, 0
+
+# W, S, E, N
+dx, dy = [-1, 0, 1, 0],[0, -1, 0, 1]
+
+# 0: W, 1: S, 2:E, 3:N
+
+time = 0
+
+def check(x,y):
+    return x==0 and y==0
+    
+for _ in range(n):
+    dir, c = tuple(input().split())
+    c = int(c)
+    if dir=='N':
+        for i in range(c):
+            time += 1
+            x, y = x+dx[3], y+dy[3]
+            if check(x,y):
+                print(time)
+    elif dir=='S':
+        for i in range(c):
+            time += 1
+            x, y = x+dx[1], y+dy[1]
+            if check(x,y):
+                print(time)
+    elif dir=='E':
+        for i in range(c):
+            time += 1
+            x, y = x+dx[2], y+dy[2]
+            if check(x,y):
+                print(time)
+    else:
+        for i in range(c):
+            time += 1
+            x, y = x+dx[0], y+dy[0]
+            if check(x,y):
+                print(time)
