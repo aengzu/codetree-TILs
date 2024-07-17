374a8f3480391913416fc5b4d96db612c04790aa
aengzu <102356873+aengzu@users.noreply.github.com>
2024-07-17 21:07:53 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240717/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,33 @@
+n, m , t = list(map(int, input().split()))
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+marbles = [tuple(map(int, input().split())) for _ in range(m)]
+
+#상하좌우
+dxs = [0,0,-1,1]
+dys = [1,-1,0,0]
+
+def in_range(x, y):
+    return x>=0 and x<n and y>=0 and y<n
+
+def can_go(x, y):
+    max_value = -1
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x+dx, y+dy
+        if in_range(nx, ny):
+            if (max_value < arr[ny][nx]):
+                max_value = arr[ny][nx]
+    return x, y
+
+new_marbles = set()
+
+for now_t in range(t):
+    for marble in marbles:
+        x, y = marble[0], marble[1]
+        new_position = can_go(x - 1, y - 1)
+        new_marbles.add((new_position[0]-1, new_position[1]-1))
+    marbles = list(new_marbles)
+
+
+print(len(new_marbles))
