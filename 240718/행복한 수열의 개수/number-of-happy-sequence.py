3028cc740a4040893404187ebbb2f406c8475d25
aengzu <102356873+aengzu@users.noreply.github.com>
2024-07-18 22:25:50 +0900

    Title: 행복한 수열의 개수, Time: 160ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240718/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,29 @@
+n, m = tuple(map(int, input().split()))
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+
+
+# 배열을 체크
+def check_in_arr(arr, m):
+    for i in range(0, n-m+1):
+        is_happy = True
+        first_num = arr[i]
+        for k in range(i, i+m):
+            if first_num != arr[k]:
+                is_happy = False
+    return is_happy
+
+
+result = 0
+for i in range(n):
+    if check_in_arr(arr[i], m):
+        result += 1
+
+
+for j in range(n):
+    col = [arr[i][j] for i in range(n)]
+    if check_in_arr(col, m):
+        result += 1
+
+print(result)
