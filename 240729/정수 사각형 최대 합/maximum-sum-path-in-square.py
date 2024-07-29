2ef0ddd3eaa5d8d51dfba63d30820ec517f3497e
aengzu <102356873+aengzu@users.noreply.github.com>
2024-07-29 20:34:46 +0900

    Title: 정수 사각형 최대 합, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240729/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240729/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -1,22 +1,18 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-# 오른쪽, 밑
-drs, dcs = [0, 1], [1, 0]
-
 
 arr_sum = [[0] * n  for _ in range(n)]
+
+# 초기값 세팅
 arr_sum[0][0] = arr[0][0]
-
-
-def in_range(r, c):
-    return 0<= r < n and 0<=c<n
 
 # 가장자리 채우기 
 for i in range(1, n):
     arr_sum[i][0] = arr_sum[i-1][0] + arr[i][0]
     arr_sum[0][i] = arr_sum[0][i-1] + arr[0][i]
 
+# 내부 채우기
 for i in range(1, n):
     for j in range(1, n):
         arr_sum[i][j] = max(arr_sum[i-1][j], arr_sum[i][j-1]) + arr[i][j]
